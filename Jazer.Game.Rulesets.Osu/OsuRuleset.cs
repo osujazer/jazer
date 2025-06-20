d5442efcc13c61d799fef43d85951a1d41a3e31e
marvin <minetoblend@gmail.com>
2025-06-20 22:44:56 +0200

    Add osu ruleset stubs

---
+++ Jazer.Game.Rulesets.Osu/OsuRuleset.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Marvin Schürz. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace Jazer.Game.Rulesets.Osu;
+
+public class OsuRuleset : IRuleset
+{
+    public string Title => "osu";
+}
---
+++ Jazer.Game/Rulesets/IRuleset.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Marvin Schürz. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace Jazer.Game.Rulesets;
+
+public interface IRuleset
+{
+    public string Title { get; }
+}
