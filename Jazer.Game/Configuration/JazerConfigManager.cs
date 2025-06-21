6bc9ee09347771eb64a368a7f68df085094977e4
James Wilson <tsunyoku@gmail.com>
2025-06-21 16:31:57 +0100

    Initialise defaults in config manager

--- Jazer.Game/Configuration/JazerConfigManager.cs
+++ Jazer.Game/Configuration/JazerConfigManager.cs
@@ -1,13 +1,19 @@
-using System.Collections.Generic;
 using osu.Framework.Configuration;
 using osu.Framework.Platform;
 
 namespace Jazer.Game.Configuration;
 
-public class JazerConfigManager(
-    Storage storage,
-    IDictionary<JazerSetting, object> defaultOverrides = null)
-    : IniConfigManager<JazerSetting>(storage, defaultOverrides);
+public class JazerConfigManager(Storage storage)
+    : IniConfigManager<JazerSetting>(storage)
+{
+    protected override void InitialiseDefaults()
+    {
+        SetDefault<string?>(JazerSetting.AuthToken, null);
+        SetDefault(JazerSetting.Username, string.Empty);
+        SetDefault(JazerSetting.SavePassword, false);
+        SetDefault(JazerSetting.SaveUsername, true);
+    }
+}
 
 public enum JazerSetting
 {
