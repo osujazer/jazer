cd993e9bb8e4e274b5ff89f5c4be967d3dda14d3
marvin <minetoblend@gmail.com>
2025-06-21 17:16:06 +0200

    Add nunito-sans fonts

---
+++ Jazer.Game/Graphics/JazerSpriteText.cs
@@ -0,0 +1,11 @@
+using osu.Framework.Graphics.Sprites;
+
+namespace Jazer.Game.Graphics;
+
+public partial class JazerSpriteText : SpriteText
+{
+    public JazerSpriteText()
+    {
+        Font = JazerFont.Default;
+    }
+}
